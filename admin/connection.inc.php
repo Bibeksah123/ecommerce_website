655077378beff9317d78b592583f1676f550fcd6
Bibek kumar sah <bibek.sah618@gmail.com>
2021-09-10 14:23:44 +0545

    Initial commit

---
+++ admin/connection.inc.php
@@ -0,0 +1,9 @@
+<?php
+session_start();
+$con=mysqli_connect("localhost","root","","ecom");
+define('SERVER_PATH',$_SERVER['DOCUMENT_ROOT'].'ecommerce_website');
+define('SITE_PATH','http://127.0.0.1/ecommerce_website/');
+
+define('PRODUCT_IMAGE_SERVER_PATH',SERVER_PATH.'media/Product/');
+define('PRODUCT_IMAGE_SITE_PATH',SITE_PATH.'media/Product/');
+?>
---
+++ footer.php
@@ -0,0 +1,133 @@
+<footer id="htc__footer">
+            <!-- Start Footer Widget -->
+            <div class="footer__container bg__cat--1">
+                <div class="container">
+                    <div class="row">
+                        <!-- Start Single Footer Widget -->
+                        <div class="col-md-3 col-sm-6 col-xs-12">
+                            <div class="footer">
+                                <h2 class="title__line--2">ABOUT US</h2>
+                                <div class="ft__details">
+                                    <p>We often talk about building relationships with acustomers, and all good relationships are built on a foundation of good communication.  We need to make our customers feel welcome and comfortable on our website and create a better experience for a customers.</p>
+                                    <div class="ft__social__link">
+                                        <ul class="social__link">
+                                            <li><a href="#"><i class="icon-social-twitter icons"></i></a></li>
+
+                                            <li><a href="#"><i class="icon-social-instagram icons"></i></a></li>
+
+                                            <li><a href="#"><i class="icon-social-facebook icons"></i></a></li>
+
+                                            <li><a href="#"><i class="icon-social-google icons"></i></a></li>
+
+                                            <li><a href="#"><i class="icon-social-linkedin icons"></i></a></li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- End Single Footer Widget -->
+                        <!-- Start Single Footer Widget -->
+                        <div class="col-md-2 col-sm-6 col-xs-12 xmt-40">
+                            <div class="footer">
+                                <h2 class="title__line--2">information</h2>
+                                <div class="ft__inner">
+                                    <ul class="ft__list">
+                                        <!-- <li><a href="#">About us</a></li> -->
+                                        <li><a href="#">Delivery Information</a></li>
+                                        <li><a href="#">Privacy & Policy</a></li>
+                                        <li><a href="#">Terms  & Condition</a></li>
+                                        <!-- <li><a href="#">Manufactures</a></li> -->
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- End Single Footer Widget -->
+                        <!-- Start Single Footer Widget -->
+                        <div class="col-md-2 col-sm-6 col-xs-12 xmt-40 smt-40">
+                            <div class="footer">
+                                <h2 class="title__line--2">my account</h2>
+                                <div class="ft__inner">
+                                    <ul class="ft__list">
+                                        <li><a href="#">My Account</a></li>
+                                        <li><a href="cart.html">My Cart</a></li>
+                                        <li><a href="#">Login</a></li>
+                                        <!-- <li><a href="wishlist.html">Wishlist</a></li> -->
+                                        <li><a href="checkout.html">Checkout</a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- End Single Footer Widget -->
+                        <!-- Start Single Footer Widget -->
+                        <!-- <div class="col-md-2 col-sm-6 col-xs-12 xmt-40 smt-40">
+                            <div class="footer">
+                                <h2 class="title__line--2">Our service</h2>
+                                <div class="ft__inner">
+                                    <ul class="ft__list">
+                                        <li><a href="#">My Account</a></li>
+                                        <li><a href="cart.html">My Cart</a></li>
+                                        <li><a href="#">Login</a></li>
+                                        <li><a href="wishlist.html">Wishlist</a></li>
+                                        <li><a href="checkout.html">Checkout</a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div> -->
+                        <!-- End Single Footer Widget -->
+                        <!-- Start Single Footer Widget -->
+                        <div class="col-md-3 col-sm-6 col-xs-12 xmt-40 smt-40">
+                            <div class="footer">
+                                <h2 class="title__line--2">contact Us </h2>
+                                <div class="ft__inner">
+                                    <div class="news__input">
+                                        <input type="text" placeholder="Enetr Your Mail*">
+                                        <div class="send__btn">
+                                            <a class="fr__btn" href="#">Send Mail</a>
+                                        </div>
+                                    </div>
+                                    
+                                </div>
+                            </div>
+                        </div>
+                        <!-- End Single Footer Widget -->
+                    </div>
+                </div>
+            </div>
+            <!-- End Footer Widget -->
+            <!-- Start Copyright Area -->
+            <div class="htc__copyright bg__cat--5">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-xs-12">
+                            <div class="copyright__inner">
+                                <p>Copyright© <a href="https://nicepage.com/website-templates">Ecommerce website templates</a> 2021. All right reserved.</p>
+                                <a href="#"><img src="images/others/Shape/paypol.png" alt="payment images"></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- End Copyright Area -->
+        </footer>
+        <!-- End Footer Style -->
+    </div>
+    <!-- Body main wrapper end -->
+
+    <!-- Placed js at the end of the document so the pages load faster -->
+
+    <!-- jquery latest version -->
+    <script src="js/vendor/jquery-3.2.1.min.js"></script>
+    <!-- Bootstrap framework js -->
+    <script src="js/bootstrap.min.js"></script>
+    <!-- All js plugins included in this file. -->
+    <script src="js/plugins.js"></script>
+    <script src="js/slick.min.js"></script>
+    <script src="js/owl.carousel.min.js"></script>
+    <!-- Waypoints.min.js. -->
+    <script src="js/waypoints.min.js"></script>
+    <!-- Main js file that contents all jQuery plugins activation. -->
+    <script src="js/main.js"></script>
+	<script src="js/custom.js"></script>
+</body>
+
+</html>
